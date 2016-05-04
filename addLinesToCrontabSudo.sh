d3072f94b633af5216ec635b0dee886298f20dc2
Ed Rogers <edwin.rogers@gmail.com>
2016-05-04 02:56:21 +0000

    Minor fix to directory structure

--- addLinesToCrontabSudo.sh
+++ addLinesToCrontabSudo.sh
@@ -2,7 +2,7 @@
 
 crontab -l > crontab_sudo.txt 2> /dev/null
 
-echo "0  3 * 4-12 1-6 /home/ed/Documents/LeafRadar/completeUpdate_Sudo.sh                 > /dev/null 2>&1" >> crontab_sudo.txt
+echo "0  3 * 4-12 1-6 /home/${USER}/LeafRadar/completeUpdate_Sudo.sh                 > /dev/null 2>&1" >> crontab_sudo.txt
 
 crontab -r 2> /dev/null
 crontab -i crontab_sudo.txt
