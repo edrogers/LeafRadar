9dfb42d8990eaa3f4e58fda34e55ff119b00349d
Ed Rogers <edwin.rogers@gmail.com>
2016-05-12 01:50:07 +0000

    Changing time to UTC for crontab

--- addLinesToCrontab.sh
+++ addLinesToCrontab.sh
@@ -10,13 +10,13 @@ crontab -l > crontab_user.txt 2> /dev/null
 echo "SHELL=${SHELL}"                                                                                                 >> crontab_user.txt
 echo "USER=${USER}"                                                                                                   >> crontab_user.txt
 echo "PATH=${PATH}"                                                                                                   >> crontab_user.txt
-echo "5        6,18 *  4-5 1-5 /home/${USER}/LeafCollection/downloadLeaf.sh                         > /dev/null 2>&1" >> crontab_user.txt
-echo "5        6,18 * 8-12 1-5 /home/${USER}/LeafCollection/downloadLeaf.sh                         > /dev/null 2>&1" >> crontab_user.txt
-echo "6        6,18 *  4-5 1-5 /home/${USER}/LeafCollection/downloadBrush.sh                        > /dev/null 2>&1" >> crontab_user.txt
-echo "6        6,18 * 8-12 1-5 /home/${USER}/LeafCollection/downloadBrush.sh                        > /dev/null 2>&1" >> crontab_user.txt
-echo "6        6,18 *    * 1-5 /home/${USER}/Weather/grabWeather.sh                                 > /dev/null 2>&1" >> crontab_user.txt
-echo "0           2 *  4-5 1-6 /home/${USER}/LeafRadar/completeUpdate_User.sh                       > /dev/null 2>&1" >> crontab_user.txt
-echo "0           2 * 8-12 1-6 /home/${USER}/LeafRadar/completeUpdate_User.sh                       > /dev/null 2>&1" >> crontab_user.txt
+echo "5       11,23 *  4-5 1-5 /home/${USER}/LeafCollection/downloadLeaf.sh                         > /dev/null 2>&1" >> crontab_user.txt
+echo "5       11,23 * 8-12 1-5 /home/${USER}/LeafCollection/downloadLeaf.sh                         > /dev/null 2>&1" >> crontab_user.txt
+echo "6       11,23 *  4-5 1-5 /home/${USER}/LeafCollection/downloadBrush.sh                        > /dev/null 2>&1" >> crontab_user.txt
+echo "6       11,23 * 8-12 1-5 /home/${USER}/LeafCollection/downloadBrush.sh                        > /dev/null 2>&1" >> crontab_user.txt
+echo "6       11,23 *    * 1-5 /home/${USER}/Weather/grabWeather.sh                                 > /dev/null 2>&1" >> crontab_user.txt
+echo "0           7 *  4-5 1-6 /home/${USER}/LeafRadar/completeUpdate_User.sh                       > /dev/null 2>&1" >> crontab_user.txt
+echo "0           7 * 8-12 1-6 /home/${USER}/LeafRadar/completeUpdate_User.sh                       > /dev/null 2>&1" >> crontab_user.txt
 
 crontab -r 2> /dev/null
 crontab -i crontab_user.txt
