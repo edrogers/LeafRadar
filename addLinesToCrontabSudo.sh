7e3b68f975f38a160480fd09341d2a81922d11c6
Ed Rogers <edwin.rogers@gmail.com>
2016-05-12 01:52:28 +0000

    Changing time to UTC for crontab

--- addLinesToCrontabSudo.sh
+++ addLinesToCrontabSudo.sh
@@ -4,7 +4,7 @@ crontab -l > crontab_sudo.txt 2> /dev/null
 
 echo "SHELL=${SHELL}"                                                                                  >> crontab_sudo.txt
 echo "PATH=${PATH}"                                                                                    >> crontab_sudo.txt
-echo "0  3 * 4-12 1-6 /home/${USER}/LeafRadar/completeUpdate_Sudo.sh                 > /dev/null 2>&1" >> crontab_sudo.txt
+echo "0  8 * 4-12 1-6 /home/${USER}/LeafRadar/completeUpdate_Sudo.sh                 > /dev/null 2>&1" >> crontab_sudo.txt
 
 crontab -r 2> /dev/null
 crontab -i crontab_sudo.txt
