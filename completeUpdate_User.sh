eb5886807a0a62d7d86f9faadebc23a62af24cea
Ed Rogers <edwin.rogers@gmail.com>
2016-12-05 16:03:12 +0000

    Adding brush CSV generation

--- completeUpdate_User.sh
+++ completeUpdate_User.sh
@@ -4,6 +4,7 @@ workingDir="/home/${USER}/LeafRadar"
 ${workingDir}/downloadLeaf.sh
 ${workingDir}/downloadBrush.sh
 ${workingDir}/generateCSV.py
+${workingDir}/generateCSV_Brush.py
 ${workingDir}/generateForecast.sh
 
 exit
