af9fbc0668c0a1ad6e2b37013c7d7acda2b5677c
Ed Rogers <edwin.rogers@gmail.com>
2016-12-05 15:48:41 +0000

    Finally committing the lines to move all the different bar charts out to the web-dir.

--- moveForecastToWeb.sh
+++ moveForecastToWeb.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-modelDir="/home/${USER}/LeafRadar"
+modelDir="/home/ubuntu/LeafRadar"
 webDir="/var/www/html"
 
 cp -p ${modelDir}/todaysForecast.png ${webDir}/
 chmod ugo+r ${webDir}/todaysForecast.png
+
+n=1;
+while [ $n -le 10 ];
+do
+   if [ $n -lt 10 ];
+   then
+      dir=$(echo "Area0${n}");
+   else
+      dir=$(echo "Area10");
+   fi
+   rsync -avz ${modelDir}/${dir}/ ${webDir}/${dir}/
+   for file in ${webDir}/${dir};
+   do
+      chmod ugo+r ${file}
+   done
+   let n+=1;
+done
 
 exit 0
