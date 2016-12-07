ea9b58de40b51d1b0f8555023aa0b3c7dde49bea
Ed Rogers <f5d2h4eeyw@snkmail.com>
2016-12-07 09:25:13 -0600

    Outline graphic at 400x400 and script to fill in outline with histogram graphic.

---
+++ Graphics/drawBoxHist.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+area=$1
+district=$2
+bizDayLow=$3
+bizDayHigh=$4
+mostLikelyDay=$5
+mostLikelyMonth=$6
+mostLikelyDate=$7
+
+baseDir="${HOME}/LeafRadar"
+imagesDir="${baseDir}/Graphics"
+fontFileRegular="${imagesDir}/JosefinSans-Regular.ttf"
+fontFileBold="${imagesDir}/JosefinSans-Bold.ttf"
+blankBoxFile="${imagesDir}/shadowbox.png"
+outputHistFile="${baseDir}/Area${area}/${district}_box_hist.png"
+
+bizDaysText=""
+if [ ${bizDayLow} -le 0 ]
+then
+    bizDaysText="'< "${bizDayHigh}" business days'"
+else
+    bizDaysText="'"${bizDayLow}"-"${bizDayHigh}" business days'";
+fi
+
+mostLikelyDayText="'${mostLikelyDay}, ${mostLikelyMonth} ${mostLikelyDate}'";
+histBarsImg="'"${baseDir}"/Area${area}/${district}_leaf_hist.png'";
+
+convert -pointsize 42 -fill black -font ${fontFileRegular} -gravity North -draw "text 0,20 'Next Collection:'" \
+                                                                          -draw "text 0,70 ${bizDaysText}" \
+        -pointsize 24                                      -gravity South -draw "text 0,45 'Most Likely Collection:'" \
+                                  -font ${fontFileBold}                   -draw "text 0,15 ${mostLikelyDayText}" \
+                                                          -gravity center -draw "image over 0,17 0,0 ${histBarsImg}" \
+   ${blankBoxFile} ${outputHistFile};
+
+
+exit 0
