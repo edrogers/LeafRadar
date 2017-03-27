e943e6ae874e492e9b08dcf54e7893f3dfbe251d
Ed Rogers <f5d2h4eeyw@snkmail.com>
2017-03-26 21:26:30 -0500

    More frequent and earlier

--- addLinesToCrontab.sh
+++ addLinesToCrontab.sh
@@ -10,11 +10,11 @@ crontab -l > crontab_user.txt 2> /dev/null
 echo "SHELL=${SHELL}"                                                                                                 >> crontab_user.txt
 echo "USER=${USER}"                                                                                                   >> crontab_user.txt
 echo "PATH=${PATH}"                                                                                                   >> crontab_user.txt
-echo "0       11,23 *  4-5 1-5 /home/${USER}/LeafCollection/downloadLeaf.sh                         > /dev/null 2>&1" >> crontab_user.txt
-echo "0       11,23 * 8-12 1-5 /home/${USER}/LeafCollection/downloadLeaf.sh                         > /dev/null 2>&1" >> crontab_user.txt
-echo "1       11,23 * 4-10 1-5 /home/${USER}/LeafCollection/downloadBrush.sh                        > /dev/null 2>&1" >> crontab_user.txt
-echo "0       11,23 *    * 1-5 /home/${USER}/Weather/grabWeather.sh                                 > /dev/null 2>&1" >> crontab_user.txt
-echo "0           7 * 4-12 1-6 /home/${USER}/LeafRadar/completeUpdate_User.sh                       > /dev/null 2>&1" >> crontab_user.txt
+echo "0       11,23 *  3-5 * /home/${USER}/LeafCollection/downloadLeaf.sh                         > /dev/null 2>&1" >> crontab_user.txt
+echo "0       11,23 * 8-12 * /home/${USER}/LeafCollection/downloadLeaf.sh                         > /dev/null 2>&1" >> crontab_user.txt
+echo "1       11,23 * 3-10 * /home/${USER}/LeafCollection/downloadBrush.sh                        > /dev/null 2>&1" >> crontab_user.txt
+echo "0       11,23 *    * * /home/${USER}/Weather/grabWeather.sh                                 > /dev/null 2>&1" >> crontab_user.txt
+echo "0           7 * 3-12 * /home/${USER}/LeafRadar/completeUpdate_User.sh                       > /dev/null 2>&1" >> crontab_user.txt
 
 crontab -r 2> /dev/null
 crontab -i crontab_user.txt
